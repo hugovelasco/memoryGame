22209436552779f418855ae63176a307d7075f1e
@hugovelasco <hugo.c.velasco@gmail.com>
2022-03-22 17:02:41 -0300

    bug fixes on setCard function

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,5 +1,45 @@
 let game = {
 
+    lockMode: false,
+    firstCard: null,
+    secondCard: null,
+
+    setCard: function (id) {
+        let card = this.cards.filter((card) => card.id === id)[0];
+        console.log(card);
+        if ((card.flipped) || (this.lockMode)) {
+            return false;
+        } else if (!this.firstCard) {
+            this.firstCard = card;
+            this.firstCard.flipped = true;
+            return true;
+        } else {
+            this.secondCard = card;
+            this.secondCard.flipped = true;
+            this.lockMode = true;
+            return true;
+        }
+    },
+
+    checkMatch: function () {
+        if (!this.firstCard || this.secondCard) {
+            return false;
+        }
+        return this.firstCard.icon === this.secondCard.icon;
+    },
+
+    clearCards: function () {
+        this.firstCard = null;
+        this.secondCard = null;
+        this.lockMode = false;
+    },
+
+    unflipCards: function () {
+        this.firstCard.flipped = false;
+        this.secondCard.flipped = false;
+        this.clearCards();
+    },
+
     carmaker: [
         "audi",
         "bmw",
@@ -14,7 +54,7 @@ let game = {
 
     cards: null,
 
-    createCardFromCarmaker: function() {
+    createCardFromCarmaker: function () {
         this.cards = [];
 
         this.carmaker.forEach((carmaker) => {
@@ -25,7 +65,7 @@ let game = {
         return this.cards;
     },
 
-    createPairFromCarmaker: function(brand) {
+    createPairFromCarmaker: function (brand) {
         return [{
             id: this.createId(brand),
             icon: brand,
@@ -37,11 +77,11 @@ let game = {
         }];
     },
 
-    createId: function(id) {
+    createId: function (id) {
         return id + "_" + parseInt(Math.random() * 1000);
     },
 
-    shuffleCards: function(cards) {
+    shuffleCards: function (cards) {
         let currentIndex = this.cards.length;
         let randomIndex = 0;
 
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -3,52 +3,72 @@ const FRONT = "card_front", BACK = "card_back", CARD = "card", ICON = "icon";
 startGame();
 
 function startGame() {
-    
-    initializeCards(game.createCardFromCarmaker());
+
+    game.createCardFromCarmaker()
+    initializeCards();
 
 }
 
-function initializeCards(cards) {
+function initializeCards() {
     let gameBoard = document.getElementById("boardGame");
 
     game.cards.forEach((card) => {
         let cardElement = document.createElement('div');
-            cardElement.id = card.id;
-            cardElement.classList.add(CARD);
-            cardElement.dataset.icon = card.icon;
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
 
-            createCardContent(card,cardElement);
+        createCardContent(card, cardElement);
 
-            cardElement.addEventListener('click',flipCard);
-            gameBoard.appendChild(cardElement);
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
     });
 }
 
-function createCardContent(card,cardElement) {
+function createCardContent(card, cardElement) {
 
     createCardFace(FRONT, card, cardElement);
     createCardFace(BACK, card, cardElement);
-    
+
 }
 
 function createCardFace(face, card, element) {
 
     let cardElementFace = document.createElement("div");
     let iconElement = document.createElement("img");
-        cardElementFace.classList.add(face);
+    cardElementFace.classList.add(face);
 
-        if (face === FRONT) {
-           iconElement.src = "assets/img/"+card.icon+".svg";
-           iconElement.classList.add(ICON);
-        } else {
-            iconElement.src = "assets/img/waze.svg";
-        }
+    if (face === FRONT) {
+        iconElement.src = "assets/img/" + card.icon + ".svg";
+        iconElement.classList.add(ICON);
+    } else {
+        iconElement.src = "assets/img/waze.svg";
+    }
 
-        cardElementFace.appendChild(iconElement);
-        element.appendChild(cardElementFace);
-    
+    cardElementFace.appendChild(iconElement);
+    element.appendChild(cardElementFace);
+
 }
 
 function flipCard() {
-    this.classList.add("flip");
+
+    if (game.setCard(this.id)) {
+
+        this.classList.add("flip");
+        if (game.secondCard) {
+            if (game.checkMatch()) {
+                game.clearCards();
+            } else {
+                setTimeout(() => {
+                    let firstCardView = document.getElementById(game.firstCard.id);
+                    let secondCardView = document.getElementById(game.secondCard.id);
+
+                    firstCardView.classList.remove("flip");
+                    secondCardView.classList.remove("flip");
+                    game.unflipCards();
+                }, 1300);
+            }
+
+        }
+    }
 };
